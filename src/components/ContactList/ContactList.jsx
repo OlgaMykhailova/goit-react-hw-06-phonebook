6db44f27dc4a817f1efd559a398eb631f06d6b74
OlgaMykhailova <rosebud_z@yahoo.com>
2022-11-16 22:23:56 +0100

    added redux toolkit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import { useSelector } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import { List } from './ContactList.styled';
+import { getContacts, getFilter } from 'redux/selectors';
+
+export const ContactList = () => {
+  const filter = useSelector(getFilter);
+  const contacts = useSelector(getContacts);
+  console.log(contacts);
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  return (
+    <List>
+      {getVisibleContacts().map(contact => (
+        <ContactListItem
+          key={contact.id}
+          name={contact.name}
+          number={contact.number}
+          id={contact.id}
+        />
+      ))}
+    </List>
+  );
+};
+
+ContactList.propTypes = {
+  visibleContacts: PropTypes.arrayOf(PropTypes.shape),
+  onDeleteContact: PropTypes.func,
+};
